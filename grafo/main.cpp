21bda3e2d96b006ff907a18f49029bba0d88736b
Fernanda Silva Martinelli Peçanha <fsmartinelli03@gmail.com>
2025-10-03 10:18:16 -0300

    Implement code changes to enhance functionality and improve performance

--- grafo/main.cpp
+++ grafo/main.cpp
@@ -12,9 +12,10 @@ int main()
 {
     int tipo;
     int tamanho;
-    cout << "Tipo de grafo" << endl;
+    cout << "--- Tipo de grafo ---" << endl;
     cout << "1 - Direcionado" << endl;
     cout << "2 - Não direcionado" << endl;
+    cout << "--------------------" << endl;
     cout << "Escolha o tipo de grafo: " << endl;
     cin >> tipo;
     cout << "Tamanho do grafo (quantidade de vertices): " << endl;
@@ -23,12 +24,14 @@ int main()
     {
     case 1:
         cout << "Grafo direcionado" << endl;
+        cout << "Tamanho do grafo: " << tamanho << endl;
         break;
     case 2:
         cout << "Grafo não direcionado" << endl;
+        cout << "Tamanho do grafo: " << tamanho << endl;
         break;
     default:
-        cout << "Opção inválida" << endl;
+        cout << "Opção tipo de grafico inválida" << endl;
     }
     return 0;
 }
